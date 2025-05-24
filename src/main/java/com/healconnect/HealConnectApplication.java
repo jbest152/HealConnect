a770732b35f1f3367f1e7395207c444e4261021d
Junz Harold Barroga <jbarroga@progesoftware.it>
2025-05-24 11:11:57 +0200

    Initialize Project

---
+++ src/main/java/com/healconnect/HealConnectApplication.java
@@ -0,0 +1,13 @@
+package com.healconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealConnectApplication.class, args);
+	}
+
+}
