b3770786a75cade36aff891944ae9f55ba79d99b
jbest152 <bjunzharold@yahoo.com>
2025-06-27 23:46:37 +0200

    Change table name into users

--- src/main/java/com/healconnect/model/User.java
+++ src/main/java/com/healconnect/model/User.java
@@ -15,8 +15,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 	
 	@Id
