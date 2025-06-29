ca85803e1476ca0d1424d86d527a24c40fe276b9
jbest152 <bjunzharold@yahoo.com>
2025-06-29 18:55:46 +0200

    Add in HomeController a way to implement User in home

--- src/main/java/com/healconnect/controller/HomeController.java
+++ src/main/java/com/healconnect/controller/HomeController.java
@@ -1,13 +1,50 @@
 package com.healconnect.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.healconnect.model.Credentials;
+import com.healconnect.model.User;
+import com.healconnect.service.CredentialsService;
+import com.healconnect.service.DoctorService;
+import com.healconnect.service.PatientService;
+import com.healconnect.service.UserService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private CredentialsService credentialsService;
+	
+	@Autowired
+	private DoctorService doctorService;
+	
+	@Autowired
+	private PatientService patientService;
 	
 	@GetMapping("/")
     public String home() {
         return "home";
     }
+	
+	@GetMapping("/success")
+	public String home(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+		Credentials credentials = credentialsService.findByUsername(userDetails.getUsername());
+		User user = credentials.getUser();
+		switch (credentials.getRole()) {
+		case ADMIN:
+			return "home-admin";
+		case DOCTOR:
+			model.addAttribute("doctor", doctorService.findByUser(user));
+			return "home-doctor";
+		case PATIENT:
+			model.addAttribute("patient", patientService.findByUser(user));
+			return "home";
+		}
+	    return "home";
+	}
 }
--- src/main/java/com/healconnect/service/PatientService.java
+++ src/main/java/com/healconnect/service/PatientService.java
@@ -1,10 +1,19 @@
 package com.healconnect.service;
+import com.healconnect.repository.PatientRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.healconnect.model.Patient;
+import com.healconnect.model.User;
 
 @Service
 public class PatientService extends GenericService<Patient, Long> {
 
+	@Autowired
+    private PatientRepository repository;
+
+	public Patient findByUser(User user) {
+		return repository.findByUser(user).get();
+	}
 }
