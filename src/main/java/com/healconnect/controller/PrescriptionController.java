dc535e3c9cfb76dd0be8aa3f5263f26a8281a1e0
jbest152 <bjunzharold@yahoo.com>
2025-06-30 22:37:38 +0200

    Add new endpoint for Prescription Controller

--- src/main/java/com/healconnect/controller/PrescriptionController.java
+++ src/main/java/com/healconnect/controller/PrescriptionController.java
@@ -1,5 +1,8 @@
 package com.healconnect.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,11 +11,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.healconnect.model.Prescription;
+import com.healconnect.model.User;
+import com.healconnect.service.DoctorService;
+import com.healconnect.service.MedicationService;
+import com.healconnect.service.UserService;
 
 @Controller
 @RequestMapping("/prescription")
 public class PrescriptionController extends GenericController<Prescription>{
 
+	@Autowired
+	private MedicationService medicationService;
+	
+	@Autowired
+	private DoctorService doctorService;
+	
+	@Autowired
+	private UserService userService;
+	
 	public PrescriptionController() {
 		super(Prescription.class);
 	}
@@ -22,6 +38,18 @@ public class PrescriptionController extends GenericController<Prescription>{
 	@GetMapping("/new")
 	public String showCreateForm(Model model){
 		return super.showCreateForm(model);
+	}
+	
+	@PreAuthorize("hasAuthority('DOCTOR')")
+	@GetMapping("/user/{id}/new")
+	public String showCreateForm(@PathVariable Long id, Model model){
+		Prescription item = new Prescription();
+		User user = userService.findById(id);
+		item.setDoctor(doctorService.findByUser(user));
+		item.setDate(LocalDate.now());
+		model.addAttribute("item", item);
+		model.addAttribute("medications", medicationService.findAll());
+		return "prescription/create";
 	}
 	
 	@Override
