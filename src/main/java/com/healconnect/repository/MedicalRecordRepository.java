5558978fb9c5a6a15ba315be0115a06bbffb5ec6
Tauriee02 <silvia.silviacolilli@outlook.it>
2025-07-02 10:46:36 +0200

    fix cartella medica

--- src/main/java/com/healconnect/repository/MedicalRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.healconnect.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.healconnect.model.MedicalRecord;
-
-public interface MedicalRecordRepository extends CrudRepository<MedicalRecord, Long> {
-
-}
